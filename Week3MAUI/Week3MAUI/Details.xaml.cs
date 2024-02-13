f3f34a6996bd384bd796ce5f6bc34d70984b575f
Sae Inoue <20103543@tafe.wa.edu.au>
2024-02-13 16:08:26 +0800

    Details stil working

---
+++ Week3MAUI/Week3MAUI/Details.xaml.cs
@@ -0,0 +1,14 @@
+namespace Week3MAUI;
+
+public partial class Details : ContentPage
+{
+	public Details()
+	{
+		InitializeComponent();
+	}
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+		Console.WriteLine("Saved!!");
+    }
+}
